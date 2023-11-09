ea4cb63c8f5eaf40b3a3f80e83504b1c81028268
Giuseppe <g.mattera@yahoo.com>
2023-11-09 15:44:55 +0100

    template_strings_2_done

---
+++ template_strings_2.js
@@ -0,0 +1,8 @@
+const myName = "Giuseppe";
+
+let myTemplateString = `Hi I'm ${myName} and 
+this is my special string for 
+exercise number two in the 
+template strings section!`;
+
+console.log(myTemplateString);
